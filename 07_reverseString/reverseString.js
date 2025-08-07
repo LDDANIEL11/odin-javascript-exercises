ca33a0fde4460babe2468882c0b8f65808f82850
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-08 03:24:07 +0330

    used for loop to pass tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splittedString = string.split("");
+  let result = "";
+  for (let i = splittedString.length - 1; i >= 0; i--) {
+    result += splittedString[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
