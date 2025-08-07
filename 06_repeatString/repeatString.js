1c0b9e08b0638106e3d0c47f98521878d36cd1de
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-08 03:09:23 +0330

    added the expected for loop inside the function to pass the tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let result = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= number; i++) {
+    result += string;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
