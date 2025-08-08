0d684bda3ad0593ef146b9d8fe686bc02a2056d5
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-08 04:33:29 +0330

    added needed codes for error handling and sumation of numbers and used a lil data structure and algo

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (start > end) {
+    const help = start;
+    start = end;
+    end = help;
+  }
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
 
+  if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  return sum;
 };
 
 // Do not edit below this line
