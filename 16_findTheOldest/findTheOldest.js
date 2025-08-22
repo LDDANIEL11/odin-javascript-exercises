488cb413a46c67ec8bc72713870e6173bf1300e2
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-22 18:57:03 +0330

    used solution , cause i dunno shit about Date() and its use

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+    //date function !(maybe library , i dunno)
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(
+      currentPerson.yearOfDeath,
+      currentPerson.yearOfBirth
+    );
+    return currentAge > oldestAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
