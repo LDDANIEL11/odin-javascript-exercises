595d3eeb24289adb3ad69e49f42873de46b40a17
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-22 16:55:39 +0330

    added the needed function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const all = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((Character) => all.includes(Character))
+    .join("");
+
+  const palindrome = cleanedString.split("").reverse().join("");
+
+  return palindrome === cleanedString;
 };
 
 // Do not edit below this line
